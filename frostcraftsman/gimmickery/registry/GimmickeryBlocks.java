c29f5dfdc267ad91a883399ac37696c8d172428d
FrostMiKu <1626652294@qq.com>
2014-07-23 15:42:15 +0800

    #4.1
    代码维护

--- frostcraftsman/gimmickery/registry/GimmickeryBlocks.java
+++ frostcraftsman/gimmickery/registry/GimmickeryBlocks.java
@@ -10,10 +10,8 @@ import net.minecraft.block.material.Material;
 public class GimmickeryBlocks {
 
 	public static BlockPowerSourceG PowerSourceBlock;
-	public static BlockPrecisionWorkBenchG PrecisionWorkBench;
 
 	public static void init() {
 		PowerSourceBlock = new BlockPowerSourceG(3500, Material.iron);
-		PrecisionWorkBench = new BlockPrecisionWorkBenchG(3501,Material.iron);
 	}
 }
