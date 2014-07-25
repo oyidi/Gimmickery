90d1db46fead3bdac27c1b746679d74924a01fc3
FrostMiKu <1626652294@qq.com>
2014-07-25 12:33:34 +0800

    #8
    楼下穿越了 = =
    成功实现实体
    添加了声音事件
    etc.

---
+++ frostcraftsman/gimmickery/item/ItemWoodKarakuriNingyG.java
@@ -0,0 +1,30 @@
+package net.frostcraftsman.gimmickery.item;
+
+import net.frostcraftsman.gimmickery.Gimmickery;
+import net.frostcraftsman.gimmickery.entity.EntityWoodKarakuriNingyG;
+import net.minecraft.entity.item.EntityTNTPrimed;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+
+public class ItemWoodKarakuriNingyG extends GimmickeryItemBase{
+
+	public ItemWoodKarakuriNingyG(int id) {
+		super(id);
+		this.st("WoodKarakuriNingy", "ƒæ÷ »À≈º");
+		this.setCreativeTab(Gimmickery.gct);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World,EntityPlayer par3EntityPlayer)
+	{
+		if(!par2World.isRemote)
+		{
+		EntityWoodKarakuriNingyG entity = new EntityWoodKarakuriNingyG(par2World, par3EntityPlayer.posX, par3EntityPlayer.posY + par3EntityPlayer.getEyeHeight(), par3EntityPlayer.posZ, par3EntityPlayer);
+		par2World.spawnEntityInWorld(entity);
+		}
+		return par1ItemStack;
+    }
+}
