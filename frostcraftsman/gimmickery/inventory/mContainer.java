c4f15a09b6b3e606d512d3326a4d26016a5e2a19
当风过时 <x269788007@gmail.com>
2014-10-31 16:04:17 +0800

    #14.10.31
    commit测试

---
+++ frostcraftsman/gimmickery/inventory/mContainer.java
@@ -0,0 +1,14 @@
+package net.frostcraftsman.gimmickery.inventory;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+
+public class mContainer extends Container {
+
+	@Override
+	public boolean canInteractWith(EntityPlayer var1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
